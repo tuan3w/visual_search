d57f4c00df4a1ef142d679b060ce2e7dd9c64cda
slothkong <mario.lr@kaist.ac.kr>
2019-05-05 16:47:46 +0200

    Renamed directories

--- visual_search/indexer/entrypoint.sh
+++ visual_search/indexer/entrypoint.sh
@@ -5,8 +5,8 @@ curl http://elasticsearch:9200/_cat/health
 
 # Set index format of elasticsearch service
 echo "Preparing elasticsearh index..."
-curl -XPUT http://elasticsearch:9200/im_data -d @es_schema.json
+curl -XPUT http://elasticsearch:9200/im_data -d @schema_es.json
 
 # And index some image data
 echo "Indexing images..."
-python es_indexer.py --input ./images --es_host http://elasticsearch --es_port 9200
+python indexer.py --input ./images --es_host http://elasticsearch --es_port 9200
